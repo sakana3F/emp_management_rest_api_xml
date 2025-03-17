20b3029058fcf3787f260e49a56209f0229351d0
harasawa <h_silvermitt@outlook.jp>
2025-03-17 16:17:38 +0900

    generatorConfiguration_1

--- src/test/kotlin/com/example/demo/AdministratorTest.kt
+++ src/test/kotlin/com/example/demo/AdministratorTest.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.junit.jupiter.api.Assertions.assertTrue
 
 @SpringBootTest
-class DatabaseTest {
+class AdministratorTest {
 
     @Test
     fun testConnection() {
